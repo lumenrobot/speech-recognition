0280124ee49bc9ab6ab7efd1ae7471a9a3607258
Hendy Irawan <ceefour666@gmail.com>
2015-11-12 14:26:09 +0700

    RAM change IP change

--- speech-recognition.sh
+++ speech-recognition.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname $0)"
-java -cp $SCRIPT_DIR'/target/dependency/*':$SCRIPT_DIR'/target/classes' org.lskk.lumen.speech.recognition.SpeechRecognitionApp "$@"
+java -Xms256m -Xmx256m -cp $SCRIPT_DIR'/target/dependency/*':$SCRIPT_DIR'/target/classes' org.lskk.lumen.speech.recognition.SpeechRecognitionApp "$@"
