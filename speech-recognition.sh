dc2513a23cfe771c79dde4f2496a90bbc7376d6f
Hendy Irawan <ceefour666@gmail.com>
2015-12-20 20:00:54 +0700

    update cmd

--- speech-recognition.sh
+++ speech-recognition.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname $0)"
-java -Xms256m -Xmx256m -cp $SCRIPT_DIR'/target/dependency/*':$SCRIPT_DIR'/target/classes' org.lskk.lumen.speech.recognition.SpeechRecognitionApp "$@"
+java -Xms256m -Xmx256m -Djava.awt.headless=true -cp $SCRIPT_DIR'/target/dependency/*':$SCRIPT_DIR'/target/classes' org.lskk.lumen.speech.recognition.SpeechRecognitionApp "$@"
